b46abc05982f436f2c2133a516d33650bce9ee05
Hritik Khurana <hritikkhurana10sm@gmail.com>
2022-08-09 16:53:38 +0530

    mongo server

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // const dbUrl = "mongodb://localhost:27017/xphone";
-const dbUrl = "mongodb+srv://<username>:<password>@cluster0.fwkqt.mongodb.net/?retryWrites=true&w=majority";
+const dbUrl = "mongodb+srv://hritikkhurana10sm:Parth@cluster0.fwkqt.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
 dbUrl : dbUrl
